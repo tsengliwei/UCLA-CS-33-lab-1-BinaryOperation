dc7cef0eea29565cad8005c113feeb33f0d24294
Li-Wei Tseng <li-weitseng@s-128-97-75-136.resnet.ucla.edu>
2014-10-15 19:37:05 -0700

    Initial Commit

---
+++ lab1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  lab1
+//
+//  Created by Li-Wei Tseng on 10/15/14.
+//  Copyright (c) 2014 Li-Wei Tseng. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
